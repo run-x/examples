84727703f730042c869ef5083b6022cf0e972453
Quinn Ngo <quinn@runx.dev>
2021-05-04 14:16:58 -0700

    Add new example

---
+++ interservice-communication/internal-service/index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/data", (req, res) => {
+  res.json({
+    source: "internal-service",
+  });
+});
+
+app.get("/healthcheck", (req, res) => {
+  res.send("ok");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
