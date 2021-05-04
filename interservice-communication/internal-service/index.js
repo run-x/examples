5f6296b7ac239c3b5a255ba2f35cd88c03841a2e
Quinn Ngo <quinn@runx.dev>
2021-05-04 14:45:05 -0700

    Try use db

--- interservice-communication/internal-service/index.js
+++ interservice-communication/internal-service/index.js
@@ -2,9 +2,16 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+const pool = new Pool();
 app.get("/data", (req, res) => {
-  res.json({
-    source: "internal-service",
+  pool.query("SELECT NOW()", (err, dbRes) => {
+    if (err) {
+      return res.code(500).json(err);
+    }
+    pool.end();
+    return res.json({
+      dbTime: dbRes,
+    });
   });
 });
 
