004a0a62003e54971d03cb4fe29b9841f6b1be7d
Quinn Ngo <quinn@runx.dev>
2021-05-04 15:02:07 -0700

    Revert

--- interservice-communication/internal-service/index.js
+++ interservice-communication/internal-service/index.js
@@ -1,18 +1,10 @@
 const express = require("express");
 const app = express();
 const port = 3000;
-const { Pool } = require("pg");
 
-const pool = new Pool();
 app.get("/data", (req, res) => {
-  pool.query("SELECT NOW()", (err, dbRes) => {
-    if (err) {
-      return res.code(500).json(err);
-    }
-    pool.end();
-    return res.json({
-      dbTime: dbRes,
-    });
+  res.send({
+    source: "internal-service",
   });
 });
 
